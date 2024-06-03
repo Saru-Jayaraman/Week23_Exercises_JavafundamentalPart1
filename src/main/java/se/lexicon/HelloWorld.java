9e7005b794a023ee70ccd6cafdcd9e09c6e7db0c
Saru-Jayaraman <sarumathijayaraman@gmail.com>
2024-06-03 15:44:52 +0200

    Committing HelloWorld.java

---
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
